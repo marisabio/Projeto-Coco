107f704f8704db6d898b2e5440dfb149422252e5
Mari Sabio <mari.msabio@posteo.net>
2026-05-24 17:14:32 -0300

    chefe rato e ajustes gerais pré playtest

--- Assets/Scripts/CatacumbasDemoManager.cs
+++ Assets/Scripts/CatacumbasDemoManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MainMenu : MonoBehaviour
+public class CatacumbasDemoManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject painelCreditos;
